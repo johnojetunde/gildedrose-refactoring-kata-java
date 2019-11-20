ce2ebc90af6812aff9accf0e3d8cbb4516c3c7d6
johnjtn <john.ojetunde@telesoftas.com>
2019-11-20 20:01:09 +0200

    test-chore: made use of the test fixture in the GildedRoseTest

--- src/test/java/com/gildedrose/domain/service/GildedRoseTest.java
+++ src/test/java/com/gildedrose/domain/service/GildedRoseTest.java
@@ -1,12 +1,12 @@
 package com.gildedrose.domain.service;
 
+import com.gildedrose.domain.fixture.ItemFixture;
 import com.gildedrose.domain.model.Item;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -22,7 +22,7 @@ class GildedRoseTest {
 
     @BeforeEach
     void setUp() {
-        items = items();
+        items = ItemFixture.itemList();
     }
 
     @Test
@@ -38,20 +38,6 @@ class GildedRoseTest {
         assertEquals("Backstage passes to a TAFKAL80ETC concert, 9, 50", items.get(6).toString());
         assertEquals("Backstage passes to a TAFKAL80ETC concert, 4, 50", items.get(7).toString());
         assertEquals("Conjured Mana Cake, 2, 4", items.get(8).toString());
-    }
-
-    private List<Item> items() {
-        return new ArrayList<Item>() {{
-            add(new Item("+5 Dexterity Vest", 10, 20));
-            add(new Item("Aged Brie", 2, 0));
-            add(new Item("Elixir of the Mongoose", -1, 7));
-            add(new Item("Sulfuras, Hand of Ragnaros", 0, 80));
-            add(new Item("Sulfuras, Hand of Ragnaros", -1, 80));
-            add(new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20));
-            add(new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49));
-            add(new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49));
-            add(new Item("Conjured Mana Cake", 3, 6));
-        }};
     }
 
 
